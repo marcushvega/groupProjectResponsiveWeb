d26bf1663e3a7b22a41e0b41abccfa82e14ab874
Ambyr La Violette <alaviolette89@gmail.com>
2017-06-28 13:12:07 -0400

    created homepage and login

--- inneed/app.js
+++ inneed/app.js
@@ -11,38 +11,56 @@ var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
-
+var port  = process.env.PORT || 3000;
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
 
+// app.use(logger('dev'));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(cookieParser());
+
+
+// app.use(function(req,res,next){
+//   console.log(req);
+//   next();
+//
+// })
+
+app.use(express.static(__dirname +'/public'));
+app.use(express.static(__dirname + '/bower_components'));
+
+
+// app.use('/', index);
+app.get('*',function(req,res,next){
+  console.log(__dirname)
+  res.sendFile(path.join(__dirname, '/public/homepage.html'));
+  // res.json({msg:'test'})
+})
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
+// app.use(function(req, res, next) {
+//   var err = new Error('Not Found');
+//   err.status = 404;
+//   next(err);
+// });
+//
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+//
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+//   console.log(err)
+// });
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
+// module.exports = app;
+app.listen(port);
+console.log(`running on ${port}`);
