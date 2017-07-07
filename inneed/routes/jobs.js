0e972a5a88c3e667629bdc06b96ed21bb56ca2d1
Philip Wofford <pwofford83@gmail.com>
2017-07-07 10:56:33 -0400

    idk

--- inneed/routes/jobs.js
+++ inneed/routes/jobs.js
@@ -1,31 +1,43 @@
 var express = require('express');
 var router = express.Router();
+//Import the mongoose module
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/test');
 
+//Set up default mongoose connection
+var mongoDB = 'mongodb://127.0.0.1/job_website';
+mongoose.connect(mongoDB);
 
-var Jobs =  mongoose.model('Jobs',{
+//Get the default connection
+var db = mongoose.connection;
+
+//Bind connection to error event (to get notification of connection errors)
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+var Jobs =  mongoose.Schema({
     companyName: {type: String,required: true },
     jobDesc: {type: String},
     openings: {type: Number},
     employmentType: {type: String},
     skills: {type: Array},
-    certs: {type: Array},
-    startDate: {type: Date},
+    certs: {type: String},
+    startDate: {type: String},
     drugTest: {type: String}
 }
-)
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  //res.json(users);
-    Jobs.find().then(function(err,jobs){
+);
+
+/* GET jobs listing. */
+router.get('/jobs', function(req, res, next) {
+    console.log('HI!');
+    window.alert("hey oh ~");
+    res.json(jobs);
+    Jobs.find().then(function(err,Jobs){
     if (err){
-        console.log(err);
+        console.log(err + Jobs + "I AM HERE");
     }
-     res.json(jobs);
+     res.json(Jobs);
     })
 });
-router.post('/', function(req, res, next) {
+router.post('/jobs', function(req, res, next) {
   //res.json(users);
     var job = new Jobs(req.body.job);
     job.save().then(function(err,jobs){
